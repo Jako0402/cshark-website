a6cd97004fe2976d8e8a2746f5f41420f5bdc070
Duplox <jakob.asbjoern.larson@gmail.com>
2026-05-11 23:44:09 +0200

    print debugging

--- js/darkmode.js
+++ js/darkmode.js
@@ -6,20 +6,24 @@ function initDarkmode() {
         themeSwitch.addEventListener("click", () => {
             darkmode = localStorage.getItem('darkmode')
             darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+            console.log("darkmode changed in")
         })
     }
 
     const enableDarkmode = () => {
         document.body.classList.add('darkmode')
         localStorage.setItem('darkmode', 'active')
+        console.log("hey, who turned off the lights?")
     }
 
     const disableDarkmode = () => {
         document.body.classList.remove('darkmode')
         localStorage.setItem('darkmode', null)
+        console.log("MY EEEEYESSS")
     }
 
     if (darkmode === "active") {
         enableDarkmode()
+        console.log("should be active")
     }
 }
